fd0b022d1fcdd1e6b1c36aeced0835228c626cd7
vraghuvaran <vraghuvaran123@gmail.com>
2019-12-25 19:23:57 +0530

    added api directory

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const app = express()
 const port = process.env.PORT || 3000
 app.use(express.static('public'))
 
+
+var obj={}
+
+obj.name="Being Zero";
+obj.college="cmrcet";
+obj.regno="17H51A05B9";
+
+app.get('/data',(req,res) => res.json(obj))
+
 app.get('/', (req, res) => res.sendFile(__dirname+"/public/html/index.html"))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
